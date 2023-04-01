70ceffb3fa2795512cfdd7adcd3d0692f742ea9f
ALFERARU Ioan-Cristian(OIB) <ioan-cristian.alferaru@ec.europa.eu>
2023-04-01 07:59:36 +0200

    feat(imdb):wip

--- src/lib/imdb.v
+++ src/lib/imdb.v
@@ -1,5 +1,15 @@
 module imdb
 import uuid {v0}
+import json
+
+pub struct Record{
+	id string
+}
+pub struct Event{
+	event_type EventType
+	id string
+	record string
+}
 
 pub enum EventType{
 	event_add
@@ -9,32 +19,55 @@ pub enum EventType{
 	event_remove_from_index
 }
 pub struct InMemDb{
-	mut:
-	indexes map[string][]string
+	pub mut:
+	indexes map[string]map[string][]string
 	indexers map[string]fn (e string) []string
 	subscribers map[string][]fn (e string)
-	pub:
+	data map[string]string
 	name string
 }
+pub fn indexer_on_field[T](val fn(r T) []string) fn (e string) []string{
+	return fn [val] [T](e string) []string{
 
+		r:=json.decode(T,e) or {
+			panic("no json for ${e}")
+		}
+		mut values:=[]string{}
+		values<<val(r)
+		return values
+	}
+}
 pub fn create_db(name string) InMemDb {
-	db:=InMemDb{
+	mut db:=InMemDb{
 		name: name
-		indexes:map[string][]string{}
+		indexes:map[string]map[string][]string{}
 		indexers:map[string]fn (e string) []string{}
 		subscribers:map[string][]fn (e string)
 	}
+	db.index_by("id",indexer_on_field[Record](fn (r Record) []string{
+	 	mut ids:=[]string{}
+		ids<<r.id
+		return ids
+	}))
+	/// db.index_by("id",fn (e string) []string{
+	/// 	r:=json.decode(Record,e) or {
+	/// 		panic("no json for ${e}")
+	/// 	}
+	/// 	mut ids:=[]string{}
+	/// 	ids<<r.id
+	/// 	return ids
+	/// })
 	return db
 }
 
 pub fn (db InMemDb) string() string{
 	return "
 	InMemDb{
-		mut:
-		${db.indexes} map[string][]string
-		${db.indexers} map[string]fn (e string) []string
-		pub:
-		${db.name} string
+		indexes ${db.indexes}
+		indexers ${db.indexers}
+		subscribers ${db.subscribers} 
+		name ${db.name} 
+		data ${db.data} 
 	}
 	"
 }
@@ -48,13 +81,49 @@ pub fn (mut db InMemDb) on(eventType string, callback fn (json string)) {
 	db.subscribers[eventType]<<(callback)
 }
 
-pub fn (mut db InMemDb) add(record string) {}
-pub fn (mut db InMemDb) remove(record string) {}
+pub fn (mut db InMemDb) add(record string) {
+	  ids:=db.indexers["id"](record)
+      db.data[ids[0]] = record
+      db.index(record)
+      db.broadcast_event("add", record)
+}
+pub fn (mut db InMemDb) remove(record string) {
+	ids:=db.indexers["id"](record)
+	id:=ids[0]
+	db.data.delete(id)
+	db.remove_from_indexes(record)
+    db.broadcast_event("remove", record)
+}
 pub fn (mut db InMemDb) update(id string, updateFn fn(json string) string) {}
 pub fn (db InMemDb) find_by_indexes(indexNames []string, indexValue string) {}
 pub fn (db InMemDb) find_by_index(indexName string, indexValue string) {}
 
-pub fn (mut db InMemDb) index(record string) {}
-pub fn (mut db InMemDb) remove_from_indexes(record string) {}
+pub fn (mut db InMemDb) index(record string) {
+	ids:=db.indexers["id"](record)
+	id:=ids[0]
+	for k,v in db.indexers {
+		println("$k => $v")
+		if !(k in db.indexes.keys()) {
+			db.indexes[k]=map[string][]string{}
+		}
+		for val in v(record){
+			db.indexes[k][val]<<id
+		}
+	}
+}
+pub fn (mut db InMemDb) remove_from_indexes(record string) {
 
-pub fn (db InMemDb) broadcast_event(eventType EventType, eventData string) {}
+	ids:=db.indexers["id"](record)
+	id:=ids[0]
+	for k,v in db.indexes {
+		println("$k => $v")
+		if !(k in db.indexes.keys()) {
+			db.indexes[k]=map[string][]string{}
+		}
+		for val in v(record){
+			db.indexes[k][val]=db.indexes[k][val].filter( fn[id](cid){return cid!=id})
+		}
+	}
+}
+
+pub fn (db InMemDb) broadcast_event(eventType string, eventData string) {}
--- src/lib/imdb_test.v
+++ src/lib/imdb_test.v
@@ -1,10 +1,26 @@
 module imdb_test
-import imdb {InMemDb,create_db}
+import imdb {InMemDb,create_db,Record,EventType}
+import json
 
+fn test_record_decode(){
+	r:=Record{
+		id:"werwer"
+	}
+	rs:=json.encode(r)
+	// println(rs)
 
+	rs1:='{"id":"werwer","anchor":{"x":10,"y":20},"size":{"x":20,"y":40}}'
+	// println(rs1)
+	r1:=json.decode(Record,rs1) or {
+		panic ("could not decode $rs1")
+	}
+	// println(r1)
+
+	assert 1==1
+}
 fn test_imdb_create(){
 	mut a:=create_db("vspace")
-	println(a.string())
+	// println(a.string())
 	assert 1==1
 }
 
@@ -20,9 +36,35 @@ fn test_imdb_on(){
 
 }
 fn test_imdb_add(){
+	mut db:=create_db("vspace")
+	for k,v in db.indexers {
+		println("$k => $v")
+	}
+
+	db.add('{"id":"werwer","anchor":{"x":10,"y":20},"size":{"x":20,"y":40}}')
+	db.add('{"id":"asdf","anchor":{"x":10,"y":20},"size":{"x":20,"y":40}}')
+	db.add('{"id":"xcvxc","anchor":{"x":10,"y":20},"size":{"x":20,"y":40}}')
+	db.add('{"id":"tyuty","anchor":{"x":10,"y":20},"size":{"x":20,"y":40}}')
+	println(db.string())
+	assert db.data.values().len == 4
 
 }
 fn test_imdb_remove(){
+	mut db:=create_db("vspace")
+	for k,v in db.indexers {
+		println("$k => $v")
+	}
+
+	db.add('{"id":"werwer","anchor":{"x":10,"y":20},"size":{"x":20,"y":40}}')
+	db.add('{"id":"asdf","anchor":{"x":10,"y":20},"size":{"x":20,"y":40}}')
+	db.add('{"id":"xcvxc","anchor":{"x":10,"y":20},"size":{"x":20,"y":40}}')
+	db.add('{"id":"tyuty","anchor":{"x":10,"y":20},"size":{"x":20,"y":40}}')
+	println(db.string())
+	assert db.data.values().len == 4
+	db.remove('{"id":"werwer","anchor":{"x":10,"y":20},"size":{"x":20,"y":40}}')
+	println(db.string())
+	assert db.data.values().len == 3
+	assert db.indexes["id"].len == 3
 
 }
 fn test_imdb_update(){
